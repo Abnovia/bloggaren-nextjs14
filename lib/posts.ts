b108cd3b33af0d68726c8d85f13420e4b919c31a
Abnovia <github@vrckalica.aleeas.com>
2025-12-07 13:49:06 +0100

    Fix formatting in fetch request headers

--- lib/posts.ts
+++ lib/posts.ts
@@ -18,9 +18,10 @@ export async function getPostByName(fileName: string): Promise<BlogPost | undefi
         headers: {
             Accept: 'application/vnd.github+json',
             Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-            'X-GitHub-Api-Version': '2022-11-28',
-        }
-    })
+        'X-GitHub-Api-Version': '2022-11-28',
+    },
+    next: { revalidate: 3600 } // Cache for 1 hour
+})
 
     if (!res.ok) return undefined
 
